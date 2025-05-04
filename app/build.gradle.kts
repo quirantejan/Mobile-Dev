016f5b92d0dbe30e7ad4e7f8d8767ebef109f2cc
ChelcRose <143089725+ChelcRose@users.noreply.github.com>
2025-05-04 23:48:27 +0800

    yay animations HAHAHA

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.airbnb.android:lottie:5.0.3")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/smartech/registerName.java
+++ app/src/main/java/com/example/smartech/registerName.java
@@ -8,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.airbnb.lottie.LottieAnimationView;
-
-public class RegisterVoicePasskey extends AppCompatActivity {
+public class registerName extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register_voice_passkey);
-
-        LottieAnimationView animationView = findViewById(R.id.lottieAnimationView);
-        animationView.setAnimation(R.raw.mic);
-        animationView.playAnimation();
-
-
+        setContentView(R.layout.activity_register_name);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-
 }
